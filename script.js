7802907a54e4f3618f1745e6a1f7dbc3906ea975
Newton Lomar <newtonlomar16@gmail.com>
2022-04-23 00:52:09 -0300

    solution to day 3

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+window.onload = () => {
+  if (JSON.parse(localStorage.getItem("people"))) {
+    JSON.parse(localStorage.getItem("people")).forEach((item) => {
+      addRowToTable(item.name, item.birthDate);
+    });
+  }
+};
+
 const form = document.querySelector("[data-js='form']");
 const datePicker = document.querySelector("[data-js='date-picker']");
 
@@ -7,15 +15,38 @@ datePicker.max = new Date()
   .split("-")
   .reverse()
   .join("-");
+
 form.addEventListener("submit", handleSubmit);
 
 function handleSubmit(event) {
   event.preventDefault();
   const nameInput = document.querySelector("[data-js='nameInput']");
+  const newPerson = {
+    name: event.target.elements["name"].value,
+    birthDate: new Date(
+      event.target.elements["birthDate"].value
+    ).toLocaleDateString(),
+  };
+  const peopleArray = JSON.parse(localStorage.getItem("people")) || [];
 
-  console.log(event.target.elements["name"].value);
-  console.log(event.target.elements["birth-date"].value);
+  peopleArray.push(newPerson);
+  localStorage.setItem("people", JSON.stringify(peopleArray));
+  addRowToTable(newPerson.name, newPerson.birthDate);
 
   event.target.reset();
   nameInput.focus();
+}
+
+function addRowToTable(name, birthDate) {
+  const tBody = document.querySelector("[data-js='tbody']");
+  const tr = document.createElement("tr");
+  const tdName = document.createElement("td");
+  const tdBirthDate = document.createElement("td");
+
+  tdName.textContent = name;
+  tdBirthDate.textContent = birthDate;
+
+  tr.appendChild(tdName);
+  tr.appendChild(tdBirthDate);
+  tBody.appendChild(tr);
 }
